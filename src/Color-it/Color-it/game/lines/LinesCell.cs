5c5951e46fa2709dc5cfe88552948bcaa61973c7
zedicov74@mail.ru <zedicov74@mail.ru>
2021-10-09 11:56:59 +0500

    Реализована модель мини-игры Lines

---
+++ src/Color-it/Color-it/game/lines/LinesCell.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+
+namespace Color_it.game.lines
+{
+    /// <summary>
+    ///     Структура данных "ячейка".
+    /// </summary>
+    class LinesCell
+    {
+        private bool is_orb, choosed, visited;
+        private Texture2D texture;
+        public Texture2D Texture { get { return texture; } set { texture = value; } }
+        public bool Choosed { get { return choosed; } set { choosed = value; } }
+        public bool IsOrb { get { return is_orb; } set { is_orb = value; } }
+        public bool Visited { get { return visited; } set { visited = value; } }
+
+        public LinesCell()
+        {
+            is_orb = false;
+            choosed = false;
+            visited = false;
+        }
+    }
+}
--- src/Color-it/Color-it/game/lines/LinesSubGame.cs
+++ src/Color-it/Color-it/game/lines/LinesSubGame.cs
@@ -19,6 +19,70 @@ namespace Color_it.game.lines
         /// </summary>
         private class LinesModel : IModel
         {
+            private const int FieldSize = 9;
+            private LinesCell[,] cells;
+            private LinesCell[] NextCells;
+            private LinesCell ChoosedCell;
+            Texture2D[] textures;
+            private int[] orbs = new int[3];
+
+            public LinesModel()
+            {
+                cells = new LinesCell[FieldSize, FieldSize];
+                NextCells = new LinesCell[3];
+            }
+            public int DeleteLines()
+            {
+                int del = 0;
+                for (int i = 0; i < FieldSize; i++)
+                {
+                    for (int j = 0; j < FieldSize; j++)
+                    {
+                        if (cells[i, j].Visited)
+                        {
+                            cells[i, j].Visited = false;
+                            cells[i, j].IsOrb = false;
+                            del++;
+                        }
+                    }
+                }
+                return del;
+            }
+            public void InsertOrbs()
+            {
+                var rand = new Random();
+                while (true)
+                {
+                    for (int i = 0; i < 3; i++)
+                    {
+                        orbs[i] = rand.Next(0, 81);
+                    }
+                    if (!(orbs[0] == orbs[1] || orbs[0] == orbs[2] || orbs[1] == orbs[2]))
+                        if (!(cells[orbs[0] / FieldSize, orbs[0] % FieldSize].IsOrb
+                            || cells[orbs[1] / FieldSize, orbs[1] % FieldSize].IsOrb
+                            || cells[orbs[2] / FieldSize, orbs[2] % FieldSize].IsOrb))
+                            break;
+                }
+                for (int i = 0; i < FieldSize; i++)
+                {
+                    for (int j = 0; j < FieldSize; j++)
+                    {
+                        for (int k = 0; k < 3; k++)
+                        {
+                            if (i == orbs[k] / FieldSize && j == orbs[k] % FieldSize)
+                            {
+                                cells[i, j].IsOrb = true;
+                                cells[i, j].Texture = NextCells[k].Texture;
+                                break;
+                            }
+                        }
+                    }
+                }
+                for (int i = 0; i < 3; i++)
+                {
+                    NextCells[i].Texture = textures[rand.Next(0, 4)];
+                }
+            }
         }
 
         /// <summary>
