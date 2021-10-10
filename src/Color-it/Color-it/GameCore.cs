c5a8c47116d2caed900dfa6aa5276529f2d9f175
Timothy Sherstobitov Sergeyevich <woodgoldfilm@gmail.com>
2021-10-10 15:14:03 +0500

    Добавил класс пользовательских настроек

--- src/Color-it/Color-it/GameCore.cs
+++ src/Color-it/Color-it/GameCore.cs
@@ -1,4 +1,8 @@
+using System.IO;
 using System.Resources;
+using System.Text;
+using System.Text.Json;
+using Microsoft.Xna.Framework.Input;
 
 namespace Color_it
 {
@@ -9,11 +13,79 @@ namespace Color_it
     /// @noop Позже приведу этот класс в нормальный вид
     public class GameCore //TODO дописать класс
     {
-        private readonly ResourceManager _resourceManager = 
+        public ResourceManager ResourceManager { get; } =
             ResourceManager.CreateFileBasedResourceManager("resources", "resources",null);
 
-        public ResourceManager ResourceManager => _resourceManager; 
+        public GameSettings Settings { get; } = GameSettings.Load();
 
         public static GameCore Core { get; }
+        
+        /// <summary>
+        /// Класс пользовательских настроек
+        /// </summary>
+        public class GameSettings
+        {
+            private static readonly string SettingsFilePath = "game_settings.json"; 
+            
+            /// <summary>
+            /// Читает настройки из файла SettingsFilePath
+            /// </summary>
+            /// <returns>Сохранённые пользователем настройки или настройки по умолчанию</returns>
+            public static GameSettings Load()
+            {
+                return JsonSerializer.Deserialize<GameSettings>(File.ReadAllText(SettingsFilePath, Encoding.UTF8));
+            }
+
+            /// <summary>
+            /// Записывает настройки в файл SettingsFilePath
+            /// </summary>
+            /// <param name="settings">Настройки для записи</param>
+            public static void save(GameSettings settings)
+            {
+                File.WriteAllText(SettingsFilePath, JsonSerializer.Serialize(settings), Encoding.UTF8);
+            }
+
+            /// <summary>
+            /// Клавиша паузы и возвращения.
+            /// По умолчанию Keys.Escape
+            /// </summary>
+            public Keys PauseAndBackKey { get; set; } = Keys.Escape;
+            
+            /// <summary>
+            /// Клавиша подтверждения и перехода вперёд
+            /// По умолчанию
+            /// </summary>
+            public Keys EnterAndForwardKey { get; set; } = Keys.Enter;
+
+            /// <summary>
+            /// Клавиша действия "Вверх"
+            /// По умолчанию Keys.Up
+            /// </summary>
+            public Keys UpKey { get; set; } = Keys.Up;
+
+            /// <summary>
+            /// Клавиша действия "Вниз"
+            /// По умолчанию Keys.Down
+            /// </summary>
+            public Keys DownKey { get; set; } = Keys.Down;
+
+            /// <summary>
+            /// Клавиша действия "Вправо"
+            /// По умолчанию Keys.Right
+            /// </summary>
+            public Keys RightKey { get; set; } = Keys.Right;
+
+            /// <summary>
+            /// Клавиша действия "Влево"
+            /// По умолчанию Keys.Left
+            /// </summary>
+            public Keys LeftKey { get; set; } = Keys.Left;
+
+            /// <summary>
+            /// Клавиша действия "Выбрать"
+            /// По умолчанию Keys.Space
+            /// </summary>
+            public Keys SelectKey { get; set; } = Keys.Space;
+        }
     }
 }
