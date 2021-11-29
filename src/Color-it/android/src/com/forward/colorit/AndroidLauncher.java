0f44d49dbcde590b1589ecc2cb112559c902d63d
Timothy Sherstobitov Sergeyevich <woodgoldfilm@gmail.com>
2021-11-29 20:50:30 +0500

    Добавил модуль для поддержки android устройств

---
+++ src/Color-it/android/src/com/forward/colorit/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.forward.colorit;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.forward.colorit.Core;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Core(), config);
+	}
+}
