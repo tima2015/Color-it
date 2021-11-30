9bd6cafb046c503eeb144b417bf8c9d8c8170e5a
Timothy Sherstobitov Sergeyevich <woodgoldfilm@gmail.com>
2021-11-30 15:20:46 +0500

    Вынес закрашивание в отдельный поток

--- src/Color-it/core/src/com/forward/colorit/coloring/ColoringImage.java
+++ src/Color-it/core/src/com/forward/colorit/coloring/ColoringImage.java
@@ -10,6 +10,7 @@ import com.badlogic.gdx.math.GridPoint2;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.scenes.scene2d.Action;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.Disposable;
 import com.forward.colorit.Core;
 
@@ -29,6 +30,7 @@ class ColoringImage extends Actor implements Disposable {
 
         pixmap = new Pixmap(Gdx.files.internal(img));
         pixmap.setBlending(Pixmap.Blending.None);
+        wbPixmap();
     }
 
     void color(ColoringMap map) {
@@ -70,6 +72,14 @@ class ColoringImage extends Actor implements Disposable {
 
     @Override
     public void dispose() {
+
+        Array<Action> actions = getActions();
+        for (Action action : actions) {
+            if (action instanceof Disposable){
+                Disposable disposable = (Disposable) action;
+                disposable.dispose();
+            }
+        }
         pixmap.dispose();
     }
 
@@ -78,52 +88,76 @@ class ColoringImage extends Actor implements Disposable {
     /**
      * Количество итераций заливки за акт
      */
-    private static final int FILLING_ITERATION = 400;
+    private static final int FILLING_ITERATION = 500;
 
     /**
      * Класс действия, реализующий анимированую заливку методом волны
      */
-    private class FillingActon extends Action {
+    private class FillingActon extends Action implements Disposable{
         private static final String TAG = "FillingActon";
+
+        private boolean disposed = false;
 
         private final Color color;
         private final Stack<GridPoint2> fillStack = new Stack<>();
 
+        private final Thread thread = new Thread(this::run);
+
         FillingActon(int x, int y, Color color) {
             this.color = color;
             fillStack.push(new GridPoint2(x, y));
+            thread.start();
             Gdx.app.debug(TAG, "Filling action start with: x = [" + x + "], y = [" + y + "], color = [" + color + "]");
         }
 
         @Override
         public boolean act(float delta) {
-            for (int i = 0; i < FILLING_ITERATION; i++) {
-                if (fillStack.empty()) {
-                    updateTexture();
+            updateTexture();
+            if (fillStack.empty()) {
                     Gdx.app.debug(TAG, "Filling action finished!");
                     return true;
-                }
-                GridPoint2 p = fillStack.pop();
-                if (p.x <= 0 || p.y <= 0 || p.x >= pixmap.getWidth() || p.y >= pixmap.getHeight()) continue;
-                int pixel = pixmap.getPixel(p.x, p.y);
-                if (pixel != color.toIntBits() && pixel == WHITE_INTS) {
-                    pixmap.setColor(color);
-                    pixmap.fillCircle(p.x, p.y, 0);
-                    List<GridPoint2> nextPoints = Arrays.asList(
-                            new GridPoint2(p.x - 1, p.y),
-                            new GridPoint2(p.x, p.y - 1),
-                            new GridPoint2(p.x, p.y + 1),
-                            new GridPoint2(p.x + 1, p.y));
-                    Collections.shuffle(nextPoints, MathUtils.random);
-                    for (GridPoint2 nextPoint : nextPoints) {
-                        fillStack.push(nextPoint);
-                    }
-                }  //!= WHITE_INTS
-
-
             }
-            updateTexture();
             return false;
+        }
+
+        @Override
+        public synchronized void dispose() {
+            disposed = true;
+        }
+
+        private synchronized void run() {
+            while (!fillStack.empty()) {
+                for (int i = 0; i < FILLING_ITERATION; i++) {
+                    if (disposed) {
+                        fillStack.clear();
+                        return;
+                    }
+                    if (fillStack.empty())
+                        return;
+                    GridPoint2 p = fillStack.pop();
+                    if (p.x <= 0 || p.y <= 0 || p.x >= pixmap.getWidth() || p.y >= pixmap.getHeight()) break;
+                    int pixel = pixmap.getPixel(p.x, p.y);
+                    if (pixel != color.toIntBits() && pixel == WHITE_INTS) {
+                        pixmap.setColor(color);
+                        pixmap.fillCircle(p.x, p.y, 0);
+                        List<GridPoint2> nextPoints = Arrays.asList(
+                                new GridPoint2(p.x - 1, p.y),
+                                new GridPoint2(p.x, p.y - 1),
+                                new GridPoint2(p.x, p.y + 1),
+                                new GridPoint2(p.x + 1, p.y));
+                        Collections.shuffle(nextPoints, MathUtils.random);
+                        for (GridPoint2 nextPoint : nextPoints) {
+                            fillStack.push(nextPoint);
+                        }
+                    }
+                }
+                try {
+                    Thread.sleep(1);
+                } catch (InterruptedException e) {
+                    Gdx.app.error(TAG, "run: ", e);
+                    return;
+                }
+            }
         }
     }
 }
