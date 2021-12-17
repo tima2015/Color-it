3f0eaff03a2c0d48547fa4048cbf11142012b3a0
skulikovs <72536774+skulikovs@users.noreply.github.com>
2021-12-17 21:15:58 +0500

    Обновил документацию

--- docs/javadoc/package-search-index.js
+++ docs/javadoc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.forward.colorit"},{"l":"com.forward.colorit.coloring"},{"l":"com.forward.colorit.lines"},{"l":"com.forward.colorit.ui"},{"l":"com.forward.colorit.ui.action"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.forward.colorit"},{"l":"com.forward.colorit.coloring"},{"l":"com.forward.colorit.lines"},{"l":"com.forward.colorit.snake"},{"l":"com.forward.colorit.tool"},{"l":"com.forward.colorit.ui"},{"l":"com.forward.colorit.ui.action"}]
